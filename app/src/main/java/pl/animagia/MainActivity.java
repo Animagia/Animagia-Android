9540dd96edc7f33cd1a526b09db85ca433f367ba
Fadion96 <nowak196@gmail.com>
2018-11-02 11:07:59 +0100

    Refactor navigation header ,add Login/Logout popup

--- app/src/main/java/pl/animagia/MainActivity.java
+++ app/src/main/java/pl/animagia/MainActivity.java
@@ -16,17 +16,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ToggleButton;
+import android.widget.*;
 
 import pl.animagia.user.Cookies;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, PopupMenu.OnMenuItemClickListener {
 
     private static final String SELECTED_ITEM = "selected item";
 
@@ -53,14 +48,13 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         final View headView = navigationView.getHeaderView(0);
-        ImageView image = headView.findViewById(R.id.login);
-        String cookie = Cookies.getCookie(Cookies.LOGIN, this);
+
         TextView textView = headView.findViewById(R.id.userEmail);
-        if (cookie.equals(Cookies.COOKIE_NOT_FOUND)){
-            textView.setText("Gość");
+        if (isLogged()) {
+            textView.setText(getUsername());
         }
         else {
-            textView.setText("Zalogowany");
+            textView.setText(R.string.guest);
         }
 
         Button button = headView.findViewById(R.id.account_view_icon_button);
@@ -68,19 +62,15 @@ public class MainActivity extends AppCompatActivity
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DialogFragment loginPopupFragment = new LoginPopupFragment();
-                loginPopupFragment.show(getSupportFragmentManager(),"popup");
-                DrawerLayout drawer = findViewById(R.id.drawer_layout);
-                drawer.closeDrawer(GravityCompat.START);
-            }
-        });
-
-        image.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                activateFragment(new LoginFragment());
-                DrawerLayout drawer = findViewById(R.id.drawer_layout);
-                drawer.closeDrawer(GravityCompat.START);
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this,v);
+                popupMenu.setOnMenuItemClickListener(MainActivity.this);
+                if (isLogged()) {
+                    popupMenu.inflate(R.menu.logged_user_menu);
+                }
+                else {
+                    popupMenu.inflate(R.menu.unlogged_user_menu);
+                }
+                popupMenu.show();
             }
         });
 
@@ -170,4 +160,43 @@ public class MainActivity extends AppCompatActivity
         return -1;
     }
 
+    private boolean isLogged(){
+        boolean logIn = false;
+        String cookie = Cookies.getCookie(Cookies.LOGIN, this);
+        System.out.println(cookie);
+        if (!cookie.equals(Cookies.COOKIE_NOT_FOUND)){
+            logIn = true;
+        }
+
+        return logIn;
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.logout) {
+            Cookies.removeCookie(Cookies.LOGIN, this);
+            Toast.makeText(this, "Wylogowano", Toast.LENGTH_SHORT).show();
+            finish();
+            overridePendingTransition(0, 0);
+            startActivity(getIntent());
+            overridePendingTransition(0, 0);
+        }
+        else if (id == R.id.login_button) {
+            activateFragment(new LoginFragment());
+            DrawerLayout drawer = findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        return true;
+    }
+
+    private String getUsername() {
+        String cookie = Cookies.getCookie(Cookies.LOGIN, this);
+        if (!cookie.equals(Cookies.COOKIE_NOT_FOUND)){
+            int first_index = cookie.indexOf('=');
+            int last_index = cookie.indexOf('%');
+            return cookie.substring(first_index + 1, last_index);
+        }
+        return "";
+    }
 }
