8909431890ada23a0fd1e4b8d59eb04c43610d9f
Krzysztof Nowak <nowak196@gmail.com>
2018-11-19 12:44:41 +0100

    Add some unit tests

---
+++ app/src/test/java/pl/animagia/VideoDataTest.java
@@ -0,0 +1,44 @@
+package pl.animagia;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VideoDataTest {
+
+    private VideoData shake;
+    private String title = "Shake-chan";
+    private String poster = "shake.jpg";
+    private String video = "syake.mkv";
+    private int numberOfEpisodes = 1;
+
+    @Before
+    public void setUp() throws Exception {
+        shake = new VideoData(title, poster, video, numberOfEpisodes);
+    }
+
+    @Test
+    public void getTitleTest() {
+        String shakeTitle = shake.getTitle();
+        assertEquals(title, shakeTitle);
+    }
+
+    @Test
+    public void getPosterAsssetUriTest() {
+        String shakePoster = shake.getPosterAsssetUri();
+        assertEquals(poster, shakePoster);
+    }
+
+    @Test
+    public void getVideoUrlTest() {
+        String shakeVideo = shake.getVideoUrl();
+        assertEquals(video,shakeVideo);
+    }
+
+    @Test
+    public void getEpisodesTest() {
+        int shakeNumberOfEpisodes = shake.getEpisodes();
+        assertEquals(numberOfEpisodes,shakeNumberOfEpisodes);
+    }
+}
