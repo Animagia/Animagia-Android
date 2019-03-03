ed8d1365eff8e4e88a5aa5df3ca9cd83fb5ed537
Tomasz Lewoc <tomasz.lewoc@protonmail.com>
2019-03-03 14:26:00 +0100

    Keep video centered on system UI show/hide (WIP)

--- app/src/main/java/pl/animagia/FullscreenPlaybackActivity.java
+++ app/src/main/java/pl/animagia/FullscreenPlaybackActivity.java
@@ -253,16 +253,18 @@ public class FullscreenPlaybackActivity extends AppCompatActivity {
     }
 
     private void hideSystemUi() {
-        mMainView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        mMainView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     }
 
     private void showSystemUi() {
-        mMainView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        mMainView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
     }
 
     private boolean isPrime(String title) {
