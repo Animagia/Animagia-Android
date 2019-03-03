31b6bc427a83c8ea99e2cf9a8ab04d9918cd809c
Tomasz Lewoc <tomasz.lewoc@protonmail.com>
2019-03-03 23:22:33 +0100

    Clean up view-related utilities

--- app/src/main/java/pl/animagia/ViewUtils.kt
+++ app/src/main/java/pl/animagia/ViewUtils.kt
@@ -1,15 +1,17 @@
 package pl.animagia
 
+import android.view.View
 import android.view.ViewGroup
 import com.google.android.exoplayer2.ui.PlayerControlView
+import com.google.android.exoplayer2.ui.PlayerView
 import java.lang.IllegalStateException
+import java.util.*
 
-fun getPlayerControlView(viewGroup: ViewGroup): PlayerControlView {
+fun getPlayerControlView(mediaPlayer: PlayerView): PlayerControlView {
 
     var controlView: PlayerControlView? = null;
 
-    for (i in 0 until viewGroup.childCount){
-        val child = viewGroup.getChildAt(i);
+    for (child in getChildrenOf(mediaPlayer)){
         if(child is PlayerControlView) {
             if(controlView != null) {
                 throw IllegalStateException("Media player has too many controllers.")
@@ -25,3 +27,18 @@ fun getPlayerControlView(viewGroup: ViewGroup): PlayerControlView {
     return controlView;
 
 }
+
+
+fun getChildrenOf(viewGroup: ViewGroup): List<View> {
+
+    val children: MutableList<View> = ArrayList();
+
+    for (i in 0 until viewGroup.childCount){
+        val child = viewGroup.getChildAt(i);
+        children.add(child);
+    }
+
+    return Collections.unmodifiableList(children);
+
+}
+
