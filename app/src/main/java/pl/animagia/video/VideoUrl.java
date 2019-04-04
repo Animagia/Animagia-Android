6e643dba0747f8ec4f95aaeaf072565a806a6a2d
marczylobartek <marczylo.b@gmail.com>
2019-04-04 19:30:44 +0200

    Reinitialize player when token is expired

--- app/src/main/java/pl/animagia/video/VideoUrl.java
+++ app/src/main/java/pl/animagia/video/VideoUrl.java
@@ -41,3 +41,43 @@ public class VideoUrl {
         return urlLine;
     }
 }
+
+ /**
+     * return message from server when link expired which start with "Ten link wygasł"
+     * otherwise return empty string
+     */
+    public static String getMessageIfLinkExpired(String html){
+        String line = getLineMessage(html);
+        int firstIndex = line.indexOf("Ten link wygasł");
+
+        String customString = "";
+        if (line.equals("")) {
+            return line;
+        } else {
+            customString =  line.substring(firstIndex);
+        }
+
+        return customString;
+    }
+
+
+    private static String getLineMessage(String html) {
+        Boolean read = true;
+        String MessageLine = "";
+        BufferedReader reader = new BufferedReader(new StringReader(html));
+        try {
+            String line = reader.readLine();
+            while(line != null && read){
+                if(line.contains("Ten link wygasł")){
+                    MessageLine = line;
+                    read = false;
+                }
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return MessageLine;
+    }
+}
