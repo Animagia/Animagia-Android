33023fc4e96f2f6d839e1d33d8e99d90bac85fe3
marczylobartek <marczylo.b@gmail.com>
2019-04-22 19:44:50 +0200

    50 issue

--- app/src/main/java/pl/animagia/CatalogFragment.java
+++ app/src/main/java/pl/animagia/CatalogFragment.java
@@ -5,13 +5,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -48,6 +51,27 @@ public class CatalogFragment extends Fragment {
                 launchPlayback(adapter.getItem(position));
             }
         });
+
+        final NavigationView navigationView = getActivity().findViewById(R.id.nav_view);
+        final View headView = navigationView.getHeaderView(0);
+
+        TextView textView = headView.findViewById(R.id.userEmail);
+        ImageView imageView = headView.findViewById(R.id.login);
+        Button button = headView.findViewById(R.id.account_view_icon_button);
+
+
+        if (isLogged()) {
+            textView.setVisibility(View.VISIBLE);
+            imageView.setVisibility(View.VISIBLE);
+            button.setVisibility(View.VISIBLE);
+            textView.setText(getUsername());
+        }
+        else {
+            textView.setVisibility(View.INVISIBLE);
+            imageView.setVisibility(View.INVISIBLE);
+            button.setVisibility(View.INVISIBLE);
+            textView.setText(R.string.guest);
+        }
     }
 
 
@@ -110,6 +134,27 @@ public class CatalogFragment extends Fragment {
 
             linearLayout.addView(textView, 0);
         }
+    }
+
+    private boolean isLogged(){
+        boolean logIn = false;
+        String cookie = Cookies.getCookie(Cookies.LOGIN, getActivity());
+        System.out.println(cookie);
+        if (!cookie.equals(Cookies.COOKIE_NOT_FOUND)){
+            logIn = true;
+        }
+
+        return logIn;
+    }
+
+    private String getUsername() {
+        String cookie = Cookies.getCookie(Cookies.LOGIN, getActivity());
+        if (!cookie.equals(Cookies.COOKIE_NOT_FOUND)){
+            int first_index = cookie.indexOf('=');
+            int last_index = cookie.indexOf('%');
+            return cookie.substring(first_index + 1, last_index);
+        }
+        return "";
     }
 
 }
