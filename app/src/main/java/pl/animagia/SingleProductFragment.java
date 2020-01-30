e613dbf07a824af4674c5d71559b2bbbd224582d
Tomasz Lewoc <tomasz.lewoc@protonmail.com>
2020-01-30 21:44:27 +0100

    Add click listener to purchase button

--- app/src/main/java/pl/animagia/SingleProductFragment.java
+++ app/src/main/java/pl/animagia/SingleProductFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SingleProductFragment extends Fragment {
 
@@ -24,5 +26,20 @@ public class SingleProductFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_single_product, null);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btn = view.findViewById(R.id.buy_film_button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
